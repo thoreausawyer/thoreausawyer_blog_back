283e04a582644e89c3e7032e1055047fa0b6f727
박동훈 <thoreausawyer@gmail.com>
2024-03-14 05:54:47 +0900

    commit for back build only + rds

--- src/main/java/com/thoreausawyer/boardback/service/implement/FileServiceImplement.java
+++ src/main/java/com/thoreausawyer/boardback/service/implement/FileServiceImplement.java
@@ -1,5 +1,7 @@
 package com.thoreausawyer.boardback.service.implement;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +14,10 @@ import org.springframework.web.multipart.MultipartFile;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import com.thoreausawyer.boardback.awsconfig.S3Config;
 import com.thoreausawyer.boardback.service.FileService;
 
@@ -25,23 +29,13 @@ public class FileServiceImplement implements FileService{
     @Value("${file.url}")
     private String fileUrl;
 
-
-    // S3 설정
-    private S3Config s3Config;
-
-    @Autowired
-    public FileServiceImplement(S3Config s3Config) {
-
-        this.s3Config = s3Config;
-    }
-
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
-
+    
+    // 안되면 복구!!
+    
     @Override
     public String upload(MultipartFile file) {
-
-        if (file.isEmpty()) return null;
+        
+            if (file.isEmpty()) return null;
 
         String originalFileName = file.getOriginalFilename();
         String extension = originalFileName.substring(originalFileName.lastIndexOf(".")); // 확장자명 가져오기.
@@ -51,32 +45,23 @@ public class FileServiceImplement implements FileService{
 
         try {
             file.transferTo(new File(savePath));
-
-            // 파일을 S3로 업로드
-            s3Config.amazonS3Client().putObject(new PutObjectRequest(bucket, saveFileName, new File(savePath)).withCannedAcl(CannedAccessControlList.PublicRead));
-            String s3Url = s3Config.amazonS3Client().getUrl(bucket, saveFileName).toString();
-            
-            // 파일을 로컬에 저장한 후에 삭제
-            new File(savePath).delete(); // 파일 삭제
-
-            return s3Url;
-
         } catch (Exception exception) {
             exception.printStackTrace();
             return null;
         }
+        
+        String url = fileUrl + saveFileName;
+        return url;
+
     }
-    
+
     @Override
     public Resource getImage(String fileName) {
+
         Resource resource = null;
-        
+
         try {
-            
-            // S3에 있는 파일을 가져오기 위해 URL 형식으로 변경
-            String s3Url = s3Config.amazonS3Client().getUrl(bucket, fileName).toString();
-            resource = new UrlResource(s3Url);
-            
+            resource = new UrlResource("file:" + filePath + fileName);            
         } catch (Exception exception) {
             exception.printStackTrace();
             return null;
@@ -84,30 +69,44 @@ public class FileServiceImplement implements FileService{
         return resource;
     }
     
-    public class S3FileChecker {
-    public static void main(String[] args) {
-        String bucketName = "your-bucket-name";
-        String objectKey = "your-object-key"; // 업로드된 파일의 키
+    
+    // // S3 설정
 
-        // AmazonS3 클라이언트 생성
-        AmazonS3 s3Client = AmazonS3ClientBuilder.standard().build();
 
-        // S3 객체 가져오기
-        S3Object object = s3Client.getObject(bucketName, objectKey);
+    //     // AWS SDK
 
-        // 객체가 존재하면 성공적으로 업로드된 것입니다.
-        System.out.println("Object found: " + object);
-        }
-    }
-    
-    
-    
-    // 안되면 복구!!
-    
+    // @Service
+    // public class S3Service {
+
+    //     @Autowired
+    //     private AmazonS3 amazonS3;
+
+    //     public List<String> listObjects(String bucketName) {
+    //         ObjectListing objectListing = amazonS3.listObjects(bucketName);
+    //         List<String> objectKeys = new ArrayList<>();
+    //         for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
+    //             objectKeys.add(objectSummary.getKey());
+    //         }
+    //         return objectKeys;
+    //     }
+    // }
+
+   
+    // private S3Config s3Config;
+
+    // @Autowired
+    // public FileServiceImplement(S3Config s3Config) {
+
+    //     this.s3Config = s3Config;
+    // }
+
+    // @Value("${cloud.aws.s3.bucket}")
+    // private String bucket;
+
     // @Override
     // public String upload(MultipartFile file) {
-        
-        //     if (file.isEmpty()) return null;
+
+    //     if (file.isEmpty()) return null;
 
     //     String originalFileName = file.getOriginalFilename();
     //     String extension = originalFileName.substring(originalFileName.lastIndexOf(".")); // 확장자명 가져오기.
@@ -117,23 +116,32 @@ public class FileServiceImplement implements FileService{
 
     //     try {
     //         file.transferTo(new File(savePath));
+
+    //         // 파일을 S3로 업로드
+    //         s3Config.amazonS3Client().putObject(new PutObjectRequest(bucket, saveFileName, new File(savePath)).withCannedAcl(CannedAccessControlList.PublicRead));
+    //         String s3Url = s3Config.amazonS3Client().getUrl(bucket, saveFileName).toString();
+            
+    //         // 파일을 로컬에 저장한 후에 삭제
+    //         new File(savePath).delete(); // 파일 삭제
+
+    //         return s3Url;
+
     //     } catch (Exception exception) {
     //         exception.printStackTrace();
     //         return null;
     //     }
-        
-    //     String url = fileUrl + saveFileName;
-    //     return url;
-
     // }
-
+    
     // @Override
     // public Resource getImage(String fileName) {
-
     //     Resource resource = null;
-
+        
     //     try {
-    //         resource = new UrlResource("file:" + filePath + fileName);            
+            
+    //         // S3에 있는 파일을 가져오기 위해 URL 형식으로 변경
+    //         String s3Url = s3Config.amazonS3Client().getUrl(bucket, fileName).toString();
+    //         resource = new UrlResource(s3Url);
+            
     //     } catch (Exception exception) {
     //         exception.printStackTrace();
     //         return null;
@@ -141,4 +149,21 @@ public class FileServiceImplement implements FileService{
     //     return resource;
     // }
     
+    // public class S3FileChecker {
+    // public static void main(String[] args) {
+    //     String bucketName = "your-bucket-name";
+    //     String objectKey = "your-object-key"; // 업로드된 파일의 키
+
+    //     // AmazonS3 클라이언트 생성
+    //     AmazonS3 s3Client = AmazonS3ClientBuilder.standard().build();
+
+    //     // S3 객체 가져오기
+    //     S3Object object = s3Client.getObject(bucketName, objectKey);
+
+    //     // 객체가 존재하면 성공적으로 업로드된 것입니다.
+    //     System.out.println("Object found: " + object);
+    //     }
+    // }
+
+
 }
